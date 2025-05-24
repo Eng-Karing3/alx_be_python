e31c73f0963f3df6c96361cafb6790cbd0bde463
Karinge Muniu <karingemuniu@gmail.com>
2025-05-24 19:39:46 +0300

    Added some changes to the variable names

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -1,10 +1,10 @@
-num1 = 10
-num2 = 5
+number1 = 10
+number2 = 5
 
-addition = num1 + num2
-substraction = num1 - num2
-multiplication = num1 * num2
+addition = number1 + number2
+substraction = number1 - number2
+multiplication = number1 * number2
 
-print(f"Addition of {num1} and {num2} is {addition}")
-print(f"Substraction of {num1} and {num2} is {substraction}")
-print(f"Multiplication of {num1} and {num2} is {multiplication}")
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Substraction of {number1} and {number2} is {substraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
