97961d452076d7e06c52fb904fae25c4d5e77b69
Karinge Muniu <karingemuniu@gmail.com>
2025-06-01 21:57:41 +0300

    Rectified some error

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,5 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
 for i in range(1, 11):
     product = number * i # type: ignore
     print(f"{number} * {i} = {product}") # type: ignore
